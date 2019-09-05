9ce238d35c1dd3be00dc61a393a6ca324634fb22
drblah <theblahblah2001@gmail.com>
2019-09-05 11:23:34 +0200

    Add apt_cache docker to speed up rebuilds due to some problems with the ubuntu mirrors. Its wil not be needed in the future. Remove ns3 source from container. From now on everything NS3 related will be handled after docker build. Add setup script which gets NS3 source code and builds it. This can be run by executing /bin/setup_ns3.sh in the container.

--- files/run.sh
+++ files/run.sh
@@ -1,3 +1,16 @@
-#!/bin/sh
-cd /workspace/source/ns-3.28
-./waf --run scratch/$1
+#!/bin/bash
+
+git clone https://gitlab.com/nsnam/bake.git /workspace/bake
+
+export BAKE_HOME=/workspace/bake
+export PATH=${PATH}:${BAKE_HOME}:${BAKE_HOME}/build/bin
+export PYTHONPATH=${PYTHONPATH}:${BAKE_HOME}:${BAKE_HOME}/build/lib
+
+# Get NS3
+cd /workspace && bake.py configure -e ns-allinone-3.30
+cd /workspace && bake.py check
+cd /workspace && bake.py download
+
+# Build NS3
+cd /workspace && bake.py build
+
