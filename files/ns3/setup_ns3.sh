54a8e484888ec713b48423966b5e3d3d894aca65
drblah <theblahblah2001@gmail.com>
2020-08-24 15:53:32 +0200

    add dependencies for dce-linux-dev

--- files/ns3/setup_ns3.sh
+++ files/ns3/setup_ns3.sh
@@ -5,9 +5,10 @@ git clone https://gitlab.com/nsnam/bake.git /workspace/bake
 export BAKE_HOME=/workspace/bake
 export PATH=${PATH}:${BAKE_HOME}:${BAKE_HOME}/build/bin
 export PYTHONPATH=${PYTHONPATH}:${BAKE_HOME}:${BAKE_HOME}/build/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BAKE_HOME}/build/lib
 
 # Get NS3
-cd /workspace && bake.py configure -e ns-allinone-3.31
+cd /workspace && bake.py configure -e dce-ns3-dev
 cd /workspace && bake.py check
 cd /workspace && bake.py download
 
