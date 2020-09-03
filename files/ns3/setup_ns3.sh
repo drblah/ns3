a5c2539168b655ae4ff3bfaf1d188d4c58e621e8
drblah <theblahblah2001@gmail.com>
2020-09-03 12:53:47 +0200

    Build using own bake and net-next-nuse repo in order to use updated batman and fixed bake config

--- files/ns3/setup_ns3.sh
+++ files/ns3/setup_ns3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://gitlab.com/nsnam/bake.git /workspace/bake
+git clone https://github.com/drblah/bake.git -b batman /workspace/bake
 
 export BAKE_HOME=/workspace/bake
 export PATH=${PATH}:${BAKE_HOME}:${BAKE_HOME}/build/bin
@@ -8,7 +8,7 @@ export PYTHONPATH=${PYTHONPATH}:${BAKE_HOME}:${BAKE_HOME}/build/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BAKE_HOME}/build/lib
 
 # Get NS3
-cd /workspace && bake.py configure -e dce-ns3-dev
+cd /workspace && bake.py configure -e dce-linux-dev-batman
 cd /workspace && bake.py check
 cd /workspace && bake.py download
 
